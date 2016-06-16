9fe1c34cf791f4fb354469422bf1410f8c675a50
rodriguesl3 <l.rsouza@outlook.com>
2016-06-16 22:41:47 +0100

    Adjusted people calculation

--- src/main/nodejs/server/users.js
+++ src/main/nodejs/server/users.js
@@ -1,19 +1,20 @@
 module.exports = {
-    getUsers: function(){
-    var people = [];
+    getUsers: function () {
+        var people = [];
 
-    var name = ["Adam", "Abe", "Maria", "Rose", "Mario", "Luigi"];
-    var surname = ["Lincoln", "Franklin", "Jackson", "Miyazaki", "M'bebe"];
+        var name = ["Adam", "Abe", "Maria", "Rose", "Mario", "Luigi"];
+        var surname = ["Lincoln", "Franklin", "Jackson", "Miyazaki", "M'bebe"];
 
-    for (var index = 0; index<=10; index++) {
-        people.push({
-        "name":name[index]+ " " + surname[index],
-        "disclosableInfo":"Lorem ipsum dolor sit amet, consectetur adipiscing elit," + 
-        "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini" +
-        "m veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo c"+
-        "onsequat."
-        });
-    }
-    return people;
+        for (var index = 0; index < 10; index++) {
+            people.push({
+                "id": index,
+                "name": name[Math.floor(Math.random() * (name.length - 1))] + " " + surname[Math.floor(Math.random() * (name.length - 1))],
+                "disclosableInfo": "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+                "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini" +
+                "m veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo c" +
+                "onsequat."
+            });
+        }
+        return people;
     }
 }
